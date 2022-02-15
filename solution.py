3f66ec2217dfefddabcc9687ef1fb98de6c22c94
pmk254 <pmk254@nyu.edu>
2022-02-14 23:06:52 -0800

    First draft

--- solution.py
+++ solution.py
@@ -3,31 +3,36 @@ from socket import *
 # In order to terminate the program
 import sys
 
-
 def webServer(port=13331):
   serverSocket = socket(AF_INET, SOCK_STREAM)
   #Prepare a server socket
   serverSocket.bind(("", port))
   #Fill in start
-
+  serverSocket.listen(1)
+  #print statement to test print('The server is ready to receive')
   #Fill in end
 
   while True:
     #Establish the connection
-    #print('Ready to serve...')
-    connectionSocket, addr = #Fill in start      #Fill in end
+    #print statement to test
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept() #Fill in start...#Fill in end
+    #print statement to test
+    print("Accepted")
     try:
 
       try:
-        message = #Fill in start    #Fill in end
+        message = connectionSocket.recv(1024) #Fill in start...#Fill in end
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = #Fill in start     #Fill in end
+        outputdata = f.read() #Fill in start...#Fill in end
         
         #Send one HTTP header line into socket.
-        #Fill in start
-
-        #Fill in end
+        # Fill in start
+        connectionSocket.send("HTTP/1.1 200 OK".encode())
+        #print statement to test
+        print("200 OK")
+        # Fill in end
 
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
@@ -37,15 +42,16 @@ def webServer(port=13331):
         connectionSocket.close()
       except IOError:
         # Send response message for file not found (404)
-        #Fill in start
-
-        #Fill in end
-
+        # Fill in start
+        connectionSocket.send("HTTP/1.1 404 Not Found".encode())
+        #print statement to test
+        print("404 Not Found")
+        # Fill in end
 
         #Close client socket
         #Fill in start
-
-        #Fill in end
+        connectionSocket.close()
+        # Fill in end
 
     except (ConnectionResetError, BrokenPipeError):
       pass
